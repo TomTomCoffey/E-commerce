bfee60fca05e0eaa8abd0ca4a5ac1f855b19bc60
TomTomCoffey <tomcoffey1017@yahoo.com>
2023-12-11 22:22:34 -0500

    Products page using temp json data

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { getProducts, type Product } from '../models/products'
 
 const pages = () => {
   return (
-    <div><div className="h1">Products</div></div>
+    <div className='product-list'>
+      <div className="h1">Products</div>
+        {getProducts().map((product: Product) => (
+          <div key={product.id}>
+            <div className="product">
+              <img src={product.thumbnail} alt={product.title} style={{width:'300px', height: '400px',objectFit: 'cover'}} />
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>
+                <span>$</span>
+                <i className="price">{product.price}</i>
+              </p>
+              <button className="btn is-primary">+</button>
+            </div>
+          </div>
+        ))}
+    </div>
   )
 }
 
